fccd502a8d94b79926ac2c16a655376f31a1cabb
Cheraws <Cheraws@gmail.com>
2013-03-16 23:25:16 -0700

    Chip not found exception added

---
+++ Board/ChipNotFoundException.java
@@ -0,0 +1,16 @@
+public Class ChipNotFoundException extends Exception{
+	/* ChipNotFoundException.java */
+
+package board;
+
+/**
+ *  Implements an Exception that says a chip cannot be found.
+ */
+  protected ChipNotFoundException() {
+    super();
+  }
+
+  protected ChipNotFoundException(String s) {
+    super(s);
+  }
+}
