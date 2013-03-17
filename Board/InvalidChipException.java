cddff8da7ab7088222972998355ee2829ed80ef9
Cheraws <Cheraws@gmail.com>
2013-03-16 23:24:32 -0700

    New exception type

---
+++ Board/InvalidChipException.java
@@ -0,0 +1,16 @@
+public Class InvalidChipException extends Exception{
+	/* InvalidNodeException.java */
+
+package board;
+
+/**
+ *  Implements an Exception that signals an attempt to use an invalid ListNode.
+ */
+  protected InvalidNodeException() {
+    super();
+  }
+
+  protected InvalidNodeException(String s) {
+    super(s);
+  }
+}
