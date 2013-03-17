84a437d397072033f54fd1d9089bc3d628008805
Cheraws <Cheraws@gmail.com>
2013-03-16 18:18:40 -0700

    Changed to Jong's version

--- Board/Board.java
+++ Board/Board.java
@@ -1,80 +1,135 @@
-	public class Board{
-		
-		protected Chip[][] board;
+                                                                     
+                                             
+/**
+ *	Board represents a single game state. It knows the 
+ *	position of each chip, and enforces the rules of the 
+ *	game, throwing an exception whenever a rule violation
+ *	is encountered.
+ *	
+ *	Invariants:
+ *  	1. Will throw an exception if a move is illegal.
+ *  	2. No two chips in any given place.
+ *  	3. Cannot be three chips adjacent to each other.
+ *  	4. Cannot place more than 10 chips of the same color.
+ *  	5. No chips in wrong goals.
+ *  	6. No chips in corners.
+ *	
+**/
 
-		/**
-		 *
-		 *	Constructs a new Board.
-		 *
-		**/
-		public Board(){
-			board = new Chip[8][8];
-
-		}
-
-		/**
-		 *
-		 *	Moves chip c to point x,y
-		 *	@param c The chip to be moved.
-		 *	@param x The x coordinate of the chip's destination
-		 *	@param y The y coordinate of the chip's destination
-		 *	@throws InvalidChipException if Chip c is not valid.
-		 *	@throws IllegalChipMoveException if the chip is moved
-		 * 			off the legal section of the board, three in a row, 
-		 *			or into the wrong goal.
-		 *
-		**/
-		public void moveChip(Chip c, int x, int y){
-			board[x][y] = null;
-		}
-
-
-		/**
-		 *
-		 *	Creates chip at x,y.
-		 *	@param x the destination x coordinate
-		 *	@param y the destination y coorinate
-		 *	@throws InvalidChipException if the chip is added
-		 *			in an illegal location (on top of another
-		 *			chip, in the wrong goal, or three in a row).
-		 *
-		**/
-		public void addChip(int x, int y) {
-
-		}
-		/**
-		 *
-		 *	Gets the chip at x,y.
-		 *	@param x the target x coordinate
-		 *	@param y the target y coordinate
-		 *	@throws ChipNotFoundException if there is no chip at x,y
-		 *	@return the Chip at x,y
-		 *
-		**/
-		public Chip getChip(int x, int y) throws ChipNotFoundException;
-
-		/**
-		 *
-		 *	Tells whether a chip at x,y exists
-		 *	@param x the target x coordinate
-		 *	@param y the target y coordinate
-		 *	@return whether there is a chip at x,y
-		 *
-		**/
-		public boolean hasChip(int x, int y);
-
-		/**
-		 *
-		 * Returns whether the game is over.
-		 * @return the color of the winning player, or Constants.NULL_PLAYER if the game is not over.
-		 *
-		**/
-		public int isGameOver();
+public class Board{	
+	/**
+	 *
+	 *	Constructs a new Board.
+	 *
+	**/
+	public int[][] board;
+	
+	public Board() {
+		board = new int[Constants.BOARDWIDTH][Constants.BOARDHEIGHT];
 	}
 
 	/**
 	 *
-	 *	The chip ADT represents a single chip on the board.
-	 *	Each chip knows its position on the board.
+	 *	Moves chip c to point x,y
+	 *	@param c The chip to be moved.
+	 *	@param x The x coordinate of the chip's destination.
+	 *	@param y The y coordinate of the chip's destination.
+	**/
+	public void moveChip(Chip c, int x, int y) {
+	
+	}
+	/**
+	 *
+	 *	Creates chip at x,y.
+	 *	@param x the destination x coordinate
+	 *	@param y the destination y coorinate
+	 *	@throws InvalidChipException if the chip is added
+	 *			in an illegal location (on top of another
+	 *			chip, in the wrong goal, or three in a row).
 	 *
 	**/
+	public void addChip(int x, int y) throws InvalidChipException;
+	/**
+	 *
+	 *	Gets the chip at x,y.
+	 *	@param x the target x coordinate
+	 *	@param y the target y coordinate
+	 *	@throws ChipNotFoundException if there is no chip at x,y
+	 *	@return the Chip at x,y
+	 *
+	**/
+	public Chip getChip(int x, int y) throws ChipNotFoundException;
+	/**
+	 *
+	 *	Tells whether a chip at x,y exists
+	 *	@param x the target x coordinate
+	 *	@param y the target y coordinate
+	 *	@return whether there is a chip at x,y
+	 *
+	**/
+	public boolean hasChip(int x, int y);
+	/**
+	 *
+	 *	Returns whether this chip is line of sight with Chip c
+	 *	@param c is the chip for comparison
+	 *	@return whether the chip is line of sight with c
+	 *
+	**/
+	public boolean isLOS(Chip a, Chip b) {
+		if (isTopBottom(a,b) || isLeftRight(a,b) || isLeftDiagonal(a,b) || isRightDiagonal(a,b)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isTopBottom(Chip a, Chip b) {
+		if (a.xPos() == b.xPos()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isLeftRight(Chip a, Chip b) {
+		if (a.yPos() == b.yPos()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isLeftDiagonal(Chip a, Chip b) {
+		int index = 0;
+		while (index + xPos() < Constants.WIDTH) {
+		
+		}
+	}
+	
+	private boolean isRightDiagonal(Chip c) {
+	
+	}
+	/**
+	 *
+	 *	Returns whether this chip is between chips c and d.
+	 *	@param c the first chip
+	 *	@param c the second chip
+	 *	@return whether this chip is between c and d.
+	 *
+	**/
+	public boolean isBetween(Chip c, Chip d) {
+	
+	}
+	/**
+	 *
+	 *	Returns the number of same-color neighbors this chip has.
+	 *	@returns the number of neighbors of this chip's color.
+	 *
+	**/
+	public int getSameColorNeighbors() {
+	
+	}
+	/**
+	 *
+	 * Returns whether the game is over.
+	 * @return the color of the winning player, or Constants.NULL_PLAYER if the game is not over.
+	 *
+	**/
+	public int isGameOver();
