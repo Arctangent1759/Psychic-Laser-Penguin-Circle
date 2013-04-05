8775ad93a56381503db94f3f7e7d5a1eb1bc8963
unknown <Cheraws@gmail.com>
2013-04-04 22:29:05 -0700

    More comments to grader.

--- DList/DList.java
+++ DList/DList.java
@@ -9,6 +9,7 @@ public class DList<T>{
 	DListNode<T> sentinel;
 	int size;
 
+        //DList Constructor
 	public DList(){
 		this.sentinel = new DListNode<T>(null,null,null,true);
 		this.sentinel.prev=this.sentinel;
@@ -16,26 +17,46 @@ public class DList<T>{
 		this.size=0;
 	}
 	
+        /**
+         *      Figures out if the function is a boolean or not.
+         *      @return a boolean stating if Dlist is empty or not.
+         */
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
+        
+        /**
+         *      Pushes the object to th front.
+         *      @param T is item being pushed to the front.
+         */ 
 	public void pushFront(T item){
 		this.sentinel.next = new DListNode<T>(item, this.sentinel, this.sentinel.next,false);
 		this.sentinel.next.next.prev = this.sentinel.next;
 		this.size++;
 	}
 
+        /**
+         *      Pushes the object to the back.
+         *      @param T is item being pushed to the back.
+         */
 	public void pushBack(T item){
 		this.sentinel.prev = new DListNode<T>(item, this.sentinel.prev, this.sentinel,false);
 		this.sentinel.prev.prev.next = this.sentinel.prev;
 		this.size++;
 	}
 
+        /**
+         *      Short form of pushback.
+         *      @param T is item being pushed to the back.
+         */
 	public void push(T item){
 		this.pushBack(item);
 	}
 
+        /**
+         *      Pops the object from the front.
+         *      @return T object being popped.
+         */
 	public T popFront(){
 		T out = this.sentinel.next.item();
 		this.sentinel.next=this.sentinel.next.next;
@@ -44,6 +65,10 @@ public class DList<T>{
 		return out;
 	}
 
+        /**
+         *      Pops the object from the back.
+         *      @return T object being popped.
+         */
 	public T popBack(){
 		T out = this.sentinel.prev.item();
 		this.sentinel.prev=this.sentinel.prev.prev;
@@ -51,11 +76,19 @@ public class DList<T>{
 		this.size--;
 		return out;
 	}
-
+        
+        /**
+         *      Pops the object in the back.
+         *      @param T is item being pushed to the back.
+         */
 	public T pop(){
 		return this.popBack();
 	}
 
+        /**
+         *      Gets the DListNode in the Front.
+         *      @return DListNode in the front.
+         */
 	public DListNode<T> getFront(){
 		if (this.sentinel.next.isSentinel){
 			return null;
@@ -63,6 +96,10 @@ public class DList<T>{
 		return this.sentinel.next;
 	}
 
+        /**
+         *      Gets the DListNode in the back.
+         *      @return DListNode in the back.
+         */
 	public DListNode<T> getBack(){
 		if (this.sentinel.prev.isSentinel){
 			return null;
@@ -70,6 +107,11 @@ public class DList<T>{
 		return this.sentinel.prev;
 	}
 
+        /**
+         *      Gets the item in a certain location.
+         *      @param index is the position to select from.
+         *      @return a T object at the index.
+         */
 	public T getItem(int index){
 		DListNode<T> curr;
 		if (index >= size || index < 0){
@@ -91,6 +133,10 @@ public class DList<T>{
 		}
 	}
 
+        /**
+         *      Appends two lists together.
+         *      @param d is a DList being added to the front.
+         */
 	public void appendFront(DList<T> d){
 		d.sentinel.next.prev=this.sentinel;
 		d.sentinel.prev.next=this.sentinel.next;
@@ -101,6 +147,11 @@ public class DList<T>{
 		d.sentinel.next=d.sentinel;
 		d.size=0;
 	}
+        
+        /**
+         *      Appends two lists together.
+         *      @param d is a DList being added to the back.
+         */
 	public void appendBack(DList<T> d){
 		d.sentinel.prev.next=this.sentinel;
 		d.sentinel.next.prev=this.sentinel.prev;
@@ -111,14 +162,27 @@ public class DList<T>{
 		d.sentinel.next=d.sentinel;
 		d.size=0;
 	}
+        
+        /**
+         *      Appends two lists together.
+         *      @param d is a DList being added to the back.
+         */
 	public void append(DList<T> d){
 		this.appendBack(d);
 	}
-
+        
+        /**
+         *      Returns length of DList.
+         *      @return the size of the Dlist.
+         */
 	public int length(){
 		return this.size;
 	}
-
+        
+        /**
+         *      Returns the toString
+         *      @return the string.
+         */
 	public String toString(){
 		DListNode<T> curr = sentinel.next;
 		String out="[";
